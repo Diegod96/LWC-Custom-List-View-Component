5d6e70dfee1899ec2a5c72a6c046946775e1dad3
Diego Delgado <ddphillyfan@gmail.com>
2020-12-14 11:58:56 -0500

    Push this branch

--- LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
+++ LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
@@ -1,30 +1,12 @@
-import {
-    LightningElement,
-    track,
-    api,
-    wire
-} from 'lwc';
-// import Case_Id from '@salesforce/schema/Case.Id';
-// import Case_Number from '@salesforce/schema/Case.Number';
-// import Priority_Level from '@salesforce/schema/Priority.Level';
-// import Case_Type from '@salesforce/schema/Case.Type';
-// import Contact_Email from '@salesforce/schema/Contact.Email';
-// import Status_Level from '@salesforce/schema/Status.Level';
-
-
+import { LightningElement, api } from 'lwc';
 export default class UpdateRecordForm extends LightningElement {
 
+    @api recordId;
+    handleSubmit(event) {
+        console.log('onsubmit event recordEditForm'+ event.detail.fields);
+    }
+    handleSuccess(event) {
+        console.log('onsuccess event recordEditForm', event.detail.id);
+    }
     
-    // @api recordId;
-    // @api objectApiName;
-
-    // fieldList = [
-    //     Case_Id, 
-    //     Case_Number, 
-    //     Priority_Level, 
-    //     Case_Type, 
-    //     Contact_Email, 
-    //     Status_Level
-    // ];
-
 }
