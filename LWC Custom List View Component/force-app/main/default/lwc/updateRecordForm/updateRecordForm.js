aa0d923730c9ae2f47d7b0ffe2128cc8c2bab95b
Diego Delgado <ddphillyfan@gmail.com>
2020-12-14 12:01:10 -0500

    Push this branch

--- LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
+++ LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
@@ -1,12 +1,4 @@
 import { LightningElement, api } from 'lwc';
 export default class UpdateRecordForm extends LightningElement {
 
-    @api recordId;
-    handleSubmit(event) {
-        console.log('onsubmit event recordEditForm'+ event.detail.fields);
-    }
-    handleSuccess(event) {
-        console.log('onsuccess event recordEditForm', event.detail.id);
-    }
-    
 }
