3b1da0064c7ef572d4ccfa90adc698bfa37c632d
Diego Delgado <ddphillyfan@gmail.com>
2020-12-13 21:42:19 -0500

    Added method to Cases Controller that allows user to query for a case by it's Id

--- LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
+++ LWC Custom List View Component/force-app/main/default/lwc/updateRecordForm/updateRecordForm.js
@@ -1,6 +1,30 @@
-import { LightningElement, track, api, wire } from 'lwc';
+import {
+    LightningElement,
+    track,
+    api,
+    wire
+} from 'lwc';
+// import Case_Id from '@salesforce/schema/Case.Id';
+// import Case_Number from '@salesforce/schema/Case.Number';
+// import Priority_Level from '@salesforce/schema/Priority.Level';
+// import Case_Type from '@salesforce/schema/Case.Type';
+// import Contact_Email from '@salesforce/schema/Contact.Email';
+// import Status_Level from '@salesforce/schema/Status.Level';
 
 
 export default class UpdateRecordForm extends LightningElement {
-}
 
+    
+    // @api recordId;
+    // @api objectApiName;
+
+    // fieldList = [
+    //     Case_Id, 
+    //     Case_Number, 
+    //     Priority_Level, 
+    //     Case_Type, 
+    //     Contact_Email, 
+    //     Status_Level
+    // ];
+
+}
